9a3d06212a27c4e4d69984c7f06c26e11507d4df
Kumari-Smitha <kumarismitha.dev@gmail.com>
2022-09-10 16:20:15 +0530

    added footer

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const inputDob = document.querySelector("#dob-input");
-const luckyNum = document.querySelector("#lucky-num");
+var luckyNum = document.querySelector("#lucky-num");
 const checkBtn = document.querySelector("#check-button");
 const textarea = document.querySelector("#text-area");
-
+console.log(document.querySelector("#lucky-num"))
 textarea.style.display = "none";
 
 const addDate = ()=>{
     let datesum = 0;
-    // console.log(inputDob.value);
+    console.log(inputDob.value.split("-"));
     for (let i = 0; i < inputDob.value.length; i++) {
         const num = parseInt(inputDob.value.charAt(i));
         if (!isNaN(num)) {
@@ -22,18 +22,29 @@ const addDate = ()=>{
 
 const checkLucky = ()=>{
     const dob = addDate();
-    const number = luckyNum.value;
-    if(dob%number === 0){
-        console.log("lucky")
+    var dateCon = inputDob.value.split("-").length;
+    var number = luckyNum.value;
+    console.log(number)
+    console.log(inputDob.value.split("-").length)
+    if(number !== "" && dateCon === 3){
+        if(dob%number === 0){
+            console.log("lucky")
+            // textarea.innerText = "";
+            textarea.style.display = "block";
+            textarea.innerText = "Yayy! "+ number+" is lucky!";
+        }else {
+            console.log("nooooooo")
+            textarea.style.display = "block";
+            textarea.innerText = "Oops! It's not lucky.";
+        }
+    } else {
+        console.log("in else")
         textarea.style.display = "block";
-        textarea.innerText = "Yayy! "+ number+" is lucky!";
-    }else {
-        console.log("nooooooo")
-        textarea.style.display = "block";
-        textarea.innerText = "Oops! It's not lucky.";
+        textarea.innerText = "Please enter both the fields";
     }
-}
-
-
-
+    
+    
+    
+    }
+    
 checkBtn.addEventListener("click", checkLucky)
